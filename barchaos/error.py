3631653cf163e890fca5835da1650c87647f53fd
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-09-08 16:35:05 -0400

    error codes

---
+++ barchaos/error.py
@@ -0,0 +1,11 @@
+
+# failure error codes
+error_codes = {
+    0: "Not processed yet",
+    1: "Success",
+    2: "Failed to estimate dt, nsteps",
+    3: "Failed to integrate orbit",
+    4: "Energy conservation criteria not met",
+    5: "SuperFreq failed on find_fundamental_frequencies()",
+    6: "Unexpected failure"
+}
