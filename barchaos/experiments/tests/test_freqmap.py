32f3bb705ed548e015a20a7e7fbc7934f1765049
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-09-09 10:07:41 -0400

    generate cache file on the fly

--- barchaos/experiments/tests/test_freqmap.py
+++ barchaos/experiments/tests/test_freqmap.py
@@ -1,12 +1,31 @@
+# Third-party
+import astropy.units as u
+import pytest
+import gala.dynamics as gd
+import numpy as np
+import h5py
 
+# Package
 from ..freqmap import FreqMap
 from ...log import logger
 
 logger.setLevel(1)
 
-def test_freqmap():
-    exp = FreqMap('/Users/adrian/projects/barchaos/tmp/cache.hdf5')
+@pytest.fixture(scope='session')
+def cache_file(tmpdir_factory):
+    fn = tmpdir_factory.mktemp('cache').join('cache.hdf5')
 
-    with FreqMap('/Users/adrian/projects/barchaos/tmp/cache.hdf5') as exp:
+    w0 = gd.PhaseSpacePosition(pos=np.random.random((3,16))*u.kpc,
+                               vel=np.random.random((3,16))*u.m/u.s)
+    with h5py.File(fn, 'w') as f:
+        g = f.create_group('w0')
+        w0.to_hdf5(g)
+
+    return str(fn)
+
+def test_freqmap(cache_file):
+    exp = FreqMap(cache_file)
+
+    with FreqMap(cache_file) as exp:
         exp.status()
         print(exp._empty_result)
