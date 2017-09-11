2c571483dc59dfc6443feaff8356246fd5ca9cf0
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-09-10 21:48:47 -0400

    iterate list

--- scripts/run.py
+++ scripts/run.py
@@ -1,3 +1,6 @@
+# Third-party
+import schwimmbad
+
 # Project
 from barchaos import experiments
 from barchaos.log import logger
@@ -19,6 +22,15 @@ if __name__ == "__main__":
                         dest='overwrite', default=False,
                         help='Destroy everything.')
 
+    # For schwimmbad / pool selection
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--ncores', dest='n_cores', default=1,
+                       type=int, help='Number of processes (uses '
+                                      'multiprocessing).')
+    group.add_argument('--mpi', dest='mpi', default=False,
+                       action='store_true', help='Run with MPI.')
+
+    # For this script
     parser.add_argument('-e', '--experiment', dest='experiment', required=True,
                         type=str, help='The name of the experiment class to '
                                        'execute.')
@@ -45,12 +57,18 @@ if __name__ == "__main__":
     else: # default
         logger.setLevel(logging.INFO)
 
+    pool = schwimmbad.choose_pool(mpi=args.mpi, processes=args.n_cores)
+
     cls = getattr(experiments, args.experiment)
 
     with cls(cache_file=args.cache_file, config_file=args.config_file,
              overwrite=args.overwrite) as exp:
 
-        tmpfile = exp(0)
-        exp.callback(tmpfile)
+        for _ in pool.map(exp, list(range(exp.n_orbits)),
+                          callback=exp.callback):
+            pass
+
         exp.status()
+
+    pool.close()
 
